7ba7ce237cf1eeac6728c3e5ec9c59563b7dd5cd
Fanlzor <91742629+Limosy-LTD@users.noreply.github.com>
2022-08-17 21:31:06 +0200

    4.0.0.F4
    1.New Country
    2.New Code

--- Contry - 2D/Assets/GameConttroler.cs
+++ Contry - 2D/Assets/GameConttroler.cs
@@ -15,6 +15,7 @@ public class GameConttroler : MonoBehaviour
         img[0].sprite = country[0];
         img[1].sprite = fieldCountry[1];
         img[2].sprite = fieldCountry[2];
+        img[3].sprite = fieldCountry[3];
     }
 
     public void OnSelectCountryMoldova()
@@ -22,6 +23,7 @@ public class GameConttroler : MonoBehaviour
         img[0].sprite = fieldCountry[0];
         img[1].sprite = country[1];
         img[2].sprite = fieldCountry[2];
+        img[3].sprite = fieldCountry[3];
     }
 
     public void OnSelectCountryRumunia()
@@ -29,5 +31,14 @@ public class GameConttroler : MonoBehaviour
         img[0].sprite = fieldCountry[0];
         img[1].sprite = fieldCountry[1];
         img[2].sprite = country[2];
+        img[3].sprite = fieldCountry[3];
+    }
+
+    public void OnSelectCountryPoland()
+    {
+        img[0].sprite = fieldCountry[0];
+        img[1].sprite = fieldCountry[1];
+        img[2].sprite = fieldCountry[2];
+        img[3].sprite = country[3];
     }
 }
