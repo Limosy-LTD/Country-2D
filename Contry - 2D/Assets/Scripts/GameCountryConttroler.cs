adedbc70a65f6c3b0b1a741fb3941113d9fd3547
Fanlzor <91742629+Limosy-LTD@users.noreply.github.com>
2022-08-18 15:56:23 +0200

    6.0.1.F6
    1.Fixed
    2.Add new

--- Contry - 2D/Assets/Scripts/GameCountryConttroler.cs
+++ Contry - 2D/Assets/Scripts/GameCountryConttroler.cs
@@ -5,6 +5,21 @@ using UnityEngine.UI;
 
 public class GameCountryConttroler : GameConttroler
 {
+    private bool showCountryFlag;
+    private bool seeMod;
+
+    [Header("ButtonSeeAll")]
+    public Image ButtonSeeAllCountry;
+
+    public Sprite SpriteOnShowCountry;
+    public Sprite SpriteOffShowCountry;
+
+    [Header("ButtonSeeMod")]
+    public Image ButtonSeeMod;
+
+    public Sprite SpriteOnSeeMod;
+    public Sprite SpriteOffSeeMod;
+
     private void Start()
     {
         if (PlayerPrefs.HasKey("ID"))
@@ -39,6 +54,141 @@ public class GameCountryConttroler : GameConttroler
             img[1].sprite = linearCountry[1]; 
             img[2].sprite = linearCountry[2]; 
             img[3].sprite = fieldCountry[3]; 
+        }
+    }
+
+    public void OnPressUkraine()
+    {
+        if(seeMod)
+        {
+            img[0].sprite = fieldCountry[0]; 
+            img[1].sprite = linearCountry[1];
+            img[2].sprite = linearCountry[2]; 
+            img[3].sprite = linearCountry[3];
+        }
+    }
+
+    public void OnPressMoldova()
+    {
+        if(seeMod)
+        {
+           img[0].sprite = linearCountry[0]; 
+           img[1].sprite = fieldCountry[1];
+           img[2].sprite = linearCountry[2]; 
+           img[3].sprite = linearCountry[3];
+        }
+    }
+
+    public void OnPressRumunia()
+    {
+        if(seeMod)
+        {
+           img[0].sprite = linearCountry[0];
+           img[1].sprite = linearCountry[1];
+           img[2].sprite = fieldCountry[2]; 
+           img[3].sprite = linearCountry[3];
+        }
+    }
+
+    public void OnPressPoland()
+    {
+        if (seeMod)
+        {
+           img[0].sprite = linearCountry[0];
+           img[1].sprite = linearCountry[1];
+           img[2].sprite = linearCountry[2]; 
+           img[3].sprite = fieldCountry[3];
+        }
+    }
+
+    public void OnSeeModTurn()
+    { 
+        seeMod =!seeMod;
+
+        if (seeMod)
+        {
+            ButtonSeeMod.sprite = SpriteOnSeeMod;
+        }
+        else if (!seeMod)
+        {
+            ButtonSeeMod.sprite = SpriteOffSeeMod;
+        }
+
+        if(idCountry == 1)
+        {   
+            img[0].sprite = fieldCountry[0];
+            img[1].sprite = linearCountry[1];
+            img[2].sprite = linearCountry[2]; 
+            img[3].sprite = linearCountry[3];  
+        }
+        else if (idCountry == 2)
+        {
+            img[0].sprite = linearCountry[0];
+            img[1].sprite = fieldCountry[1];
+            img[2].sprite = linearCountry[2]; 
+            img[3].sprite = linearCountry[3]; 
+        }
+        else if (idCountry == 3)
+        {
+            img[0].sprite = linearCountry[0];
+            img[1].sprite = linearCountry[1];
+            img[2].sprite = fieldCountry[2]; 
+            img[3].sprite = linearCountry[3]; 
+        }
+        else if (idCountry == 4)
+        {
+            img[0].sprite = linearCountry[0];
+            img[1].sprite = linearCountry[1];
+            img[2].sprite = linearCountry[2]; 
+            img[3].sprite = fieldCountry[3]; 
+        }
+    }
+
+    public void OnShowAllCountryFlag()
+    {
+        if (!showCountryFlag)
+        {   
+            img[0].sprite = fieldCountry[0];
+            img[1].sprite = fieldCountry[1];
+            img[2].sprite = fieldCountry[2]; 
+            img[3].sprite = fieldCountry[3];
+
+            showCountryFlag = true;
+            ButtonSeeAllCountry.sprite = SpriteOnShowCountry;
+        }
+        else if (showCountryFlag)
+        {
+            if(idCountry == 1)
+            {   
+                img[0].sprite = fieldCountry[0];
+                img[1].sprite = linearCountry[1];
+                img[2].sprite = linearCountry[2]; 
+                img[3].sprite = linearCountry[3];  
+            }
+            else if (idCountry == 2)
+            {
+                img[0].sprite = linearCountry[0];
+                img[1].sprite = fieldCountry[1];
+                img[2].sprite = linearCountry[2]; 
+                img[3].sprite = linearCountry[3]; 
+            }
+            else if (idCountry == 3)
+            {
+                img[0].sprite = linearCountry[0];
+                img[1].sprite = linearCountry[1];
+                img[2].sprite = fieldCountry[2]; 
+                img[3].sprite = linearCountry[3]; 
+            }
+            else if (idCountry == 4)
+            {
+                img[0].sprite = linearCountry[0];
+                img[1].sprite = linearCountry[1];
+                img[2].sprite = linearCountry[2]; 
+                img[3].sprite = fieldCountry[3]; 
+            }
+
+            showCountryFlag = false;
+            ButtonSeeAllCountry.sprite = SpriteOffShowCountry;
         }
     }
 }
